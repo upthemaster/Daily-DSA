aa00b5bdea4e1c27042e593081d24b69c0d37a20
upthemaster <upthemaster@gmail.com>
2025-11-02 00:01:23 +0530

    ArrayFunction

--- ARRAY/ArrayFunction.java
+++ ARRAY/ArrayFunction.java
@@ -1,9 +1,8 @@
 public class ArrayFunction {
-    public static void update(int marks []){
-        for(int i = 0; i<marks.length; i++){
+    public static void update(int marks []) {
+        for(int i = 0; i<marks.length; i++) {
             marks[i] = marks[i]+1;
         }
-
     }
     public static void main(String[] args) {
         int marks[] = {97,98,99};
