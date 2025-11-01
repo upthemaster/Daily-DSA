8da9e16afcbbd5742ee20be311e84e4ffa953ad0
upthemaster <upthemaster@gmail.com>
2025-11-02 00:09:23 +0530

    ReversingArrayElements

--- ARRAY/ReverseArray.java
+++ ARRAY/ReverseArray.java
@@ -1,5 +1,5 @@
 public class ReverseArray {
-    public static void reverse(int []nums){
+    public static void reverse(int []nums) {
         int first = 0, last = nums.length-1;
         while(first<last){
             int temp = nums[last];
@@ -13,7 +13,7 @@ public class ReverseArray {
         int []nums = {2,4,6,8,10};
         reverse(nums);
 
-        for(int i = 0;i<nums.length;i++){
+        for(int i = 0;i<nums.length;i++) {
             System.out.print(nums[i]+" ");
             
         }
