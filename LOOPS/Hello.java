6713333bc5b140fc1818829213f0f3a1d04904f2
upthemaster <upthemaster@gmail.com>
2025-11-03 23:48:29 +0530

    PrintingHelloMultipleTimesUsingForLoop

--- LOOPS/Hello.java
+++ LOOPS/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
-    public static void main(String [] args){
+    public static void main(String [] args) {
         int counter = 0;
         while(counter < 100){
             System.out.println("hello world");
