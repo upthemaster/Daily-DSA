a4a3cc525cc96403daaad969a7e8a37200a599d1
upthemaster <upthemaster@gmail.com>
2025-11-03 23:51:12 +0530

    ReversingTheInputNumber

--- LOOPS/PrintReverse.java
+++ LOOPS/PrintReverse.java
@@ -1,7 +1,7 @@
 public class PrintReverse {
     public static void main(String[] args) {
         int n = 85469;
-        while(n > 0){
+        while(n > 0) {
             int lastDigit = n%10;
             System.out.print(lastDigit);
             n = n/10;
