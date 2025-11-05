00e9ce3ec306805a2533a2ecf0b0857735d15fef
upthemaster <upthemaster@gmail.com>
2025-11-05 23:13:29 +0530

    SolvingBinomialQuestionUsingMethods

--- METHODS/BinomialCoefficient.java
+++ METHODS/BinomialCoefficient.java
@@ -6,14 +6,13 @@ public class BinomialCoefficient {
         }
         return f;
     }
-    public static int BinCoeff(int n, int r){
+    public static int BinCoeff(int n, int r) {
         int n_fact = factorial(n);
         System.out.println("Factorial of n:"+n_fact);
         int r_fact = factorial(r);
         System.out.println("Factorial of r:"+r_fact);
         int nmr = factorial(n-r);
         System.out.println("Factorial of nmr:"+nmr);
-
         int BinCoeff = n_fact/(r_fact * nmr);
         return BinCoeff;
     }
