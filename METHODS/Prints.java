61d0ba032088242ecfb7587fd7f3ab73fd0678aa
upthemaster <upthemaster@gmail.com>
2025-11-05 23:17:42 +0530

    BasicPrintMethod

--- METHODS/Prints.java
+++ METHODS/Prints.java
@@ -1,5 +1,5 @@
 public class Prints{
-    public static void printhw(){
+    public static void printhw() {
         System.out.println("Hello World!");
     }
     public static void main(String [] args){
