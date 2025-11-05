33a5957ed901b43ccb1812a66d226d2158f42661
upthemaster <upthemaster@gmail.com>
2025-11-05 23:16:36 +0530

    FunctionOverriding

--- METHODS/FunOver.java
+++ METHODS/FunOver.java
@@ -2,10 +2,10 @@ public class FunOver {
     public static int sum(int a, int b){
         return a+b;
     }
-
     public static int sum(int a, int b, int c){
         return a+b+c;
     }
+    
     public static void main(String args[]){
         System.out.println(sum(5,4));//function1
         System.out.println(sum(5,4,10));//function2
