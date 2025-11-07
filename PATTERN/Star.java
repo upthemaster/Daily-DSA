7376832e58564740ed545ee68434db58b731012a
upthemaster <upthemaster@gmail.com>
2025-11-07 23:16:02 +0530

    BasicStarPatternUsingForLoop

--- PATTERN/Star.java
+++ PATTERN/Star.java
@@ -4,6 +4,7 @@ public class Star{
         for(int i = 1; i<= 4; i++){
             for(int j = 1; j<=i;j++){
                 System.out.print("*");
+                
             }
                 System.out.println("");
         }
