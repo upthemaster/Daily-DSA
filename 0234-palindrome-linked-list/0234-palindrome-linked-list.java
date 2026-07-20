eaeeec69abe46e7b364e3057270c8fa0488da51c
upthemaster <upthemaster@gmail.com>
2026-07-20 23:53:30 +0530

    Time: 5 ms (43.86%), Space: 94.3 MB (67.61%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,25 +1,42 @@
-class Solution { // Approach : Slow & Fast Pointer  Time : O(n)     Space : O(1)
-    public boolean isPalindrome(ListNode head) {
-        ListNode slow = head, fast = head, prev, temp;
+class Solution { //Approach : Slow & Fast
+    //find mid
+    public ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-        prev = slow;
-        slow = slow.next;
-        prev.next = null;
-        while (slow != null) {
-            temp = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = temp;
+        return slow;
+    }
+    //check palindrome
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null) {
+            return true;
         }
-        fast = head;
-        slow = prev;
-        while (slow != null) {
-            if (fast.val != slow.val) return false;
-            fast = fast.next;
-            slow = slow.next;
+
+        ListNode midNode = findMid(head); // find middle
+        //reverse a second half
+        ListNode prev = null;
+        ListNode curr = midNode;
+        ListNode next;
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode right = prev;
+        ListNode left = head;
+        //check both half are equal or not
+        while(right != null) {
+            if(left.val != right.val) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
         }
         return true;
     }
