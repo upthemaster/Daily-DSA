f3ba3b73d094f63d0a170e788009372101a1e6af
upthemaster <upthemaster@gmail.com>
2026-07-22 20:00:49 +0530

    Time: 0 ms (100%), Space: 46.8 MB (30.57%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -1,26 +1,26 @@
-public class Solution {// Approach : S & F Pointer  Time : O(n)     Space : O(1)
-
+public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if(head == null || head.next == null)
+        if(head == null || head.next == null) {
             return null;
-        
+        }
         ListNode slow = head;
         ListNode fast = head;
 
-        while(fast != null && fast.next != null){
+        while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
 
-            if(slow == fast){
+            if(slow == fast) {
                 slow = head;
-                while( slow != fast) {
+
+                while(slow != fast) {
                     slow = slow.next;
                     fast = fast.next;
                 }
                 return slow;
-            }
+            } 
         }
-        return null;  
+        return null;
     }
 }
 
