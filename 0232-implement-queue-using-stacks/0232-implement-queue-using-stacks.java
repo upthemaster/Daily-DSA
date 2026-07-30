bd7f37a1ab22dd21edbbf043aa23908066b6f752
upthemaster <upthemaster@gmail.com>
2026-07-30 16:05:41 +0530

    Time: 1 ms (28.43%), Space: 43.1 MB (15.41%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class MyQueue {
+
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+    
+    public void push(int x) {
+        while(!s1.empty()) {
+            s2.push(s1.pop());
+        }
+        s1.push(x);
+        while(!s2.empty()) {
+            s1.push(s2.pop());
+        }
+    }
+    
+    public int pop() {
+        peek();
+        return s1.pop();
+    }
+    
+    public int peek() {
+        if(empty()) {
+            return -1;
+        }
+        return s1.peek();
+    }
+    
+    public boolean empty() {
+        return s1.empty();
+    }
+}
