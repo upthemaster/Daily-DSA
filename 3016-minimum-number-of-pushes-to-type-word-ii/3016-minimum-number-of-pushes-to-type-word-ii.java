dc28c14946f38065dec89469ded9b379358a6167
upthemaster <upthemaster@gmail.com>
2026-07-31 09:45:16 +0530

    Time: 10 ms (66.76%), Space: 48.2 MB (32.08%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minimumPushes(String word) {
+        int []freq = new int[26];
+
+        for(char ch : word.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        Arrays.sort(freq);
+        int push = 0;
+        int index = 0;
+
+        for(int i = 25; i >= 0; i--) {
+            if(freq[i] == 0){
+                break;
+            }
+            
+            push = push + freq[i] * ((index/8) + 1);
+            index++;
+        }
+        return push;
+    }
+}
