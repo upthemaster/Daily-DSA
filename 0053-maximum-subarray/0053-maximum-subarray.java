0c4a4872fcf98a4a76d7dd58d4f23235e6c95074
upthemaster <upthemaster@gmail.com>
2026-09-02 20:51:03 +0530

    Time: 1 ms (99.94%), Space: 77.3 MB (32.54%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,15 +1,16 @@
 class Solution { // kadane's Algo O(n)
     public int maxSubArray(int[] nums) {
         int bestEnding = nums[0];
-        int finalAns = nums[0];
+        int finalRes = nums[0];
 
         for(int i = 1; i < nums.length; i++) {
             int val1 = bestEnding + nums[i];
             int val2 = nums[i];
 
             bestEnding = Math.max(val1, val2);
-            finalAns = Math.max(finalAns, bestEnding);
+            finalRes = Math.max(bestEnding, finalRes);
         }
-        return finalAns;
+
+        return finalRes;
     }
 }
